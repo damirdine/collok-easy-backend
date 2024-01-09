0e9fe7c6bc4d403e212ca272e7b3089fa574acd9
Damirdine ALI SOILIHI <damirdine@outlook.fr>
2024-01-09 21:37:33 +0100

    config - password token secret and email credentials for nodemailer

---
+++ app/helpers/email.js
@@ -0,0 +1,19 @@
+import { createTransport } from "nodemailer";
+import { GMAIL_EMAIL, GMAIL_PASSWORD } from "./constant";
+
+const transporter = createTransport({
+  service: "gmail",
+  auth: {
+    user: GMAIL_EMAIL, // replace with your Gmail email
+    pass: GMAIL_PASSWORD, // replace with your Gmail password
+  },
+});
+
+export function sendEmail(email, content) {
+  return transporter.sendMail({
+    from: `"L'equipe support 📞" <${GMAIL_EMAIL}>`,
+    to: email,
+    subject: "Récupération de mot de passe",
+    html: content,
+  });
+}
