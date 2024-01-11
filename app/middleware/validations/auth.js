6c9bd9790b101fd7f604b43c34d2222a5bc02340
Damirdine ALI SOILIHI <damirdine@outlook.fr>
2024-01-11 15:02:29 +0100

    config : password validator

--- app/middleware/validations/auth.js
+++ app/middleware/validations/auth.js
@@ -1,18 +1,31 @@
 import { check } from "express-validator";
 import handleValidationErrors from "./index.js";
+import { translate } from "../../helpers/translate.js";
+
+const isStrongPassword = (value) => {
+  // au moins une majuscule, un chiffre et un caractère spécial
+  const strongPasswordRegex =
+    /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
+  return strongPasswordRegex.test(value);
+};
+
+const checkPassword = check("password").custom((value) => {
+  if (!isStrongPassword(value)) {
+    throw new Error(translate("errors.password_no_valid"));
+  }
+  return true;
+});
 
 const authValidator = {
-  login: [check("email").isEmail(), check("password").isLength({ min: 8 })],
-  register: [check("email").isEmail(), check("password").isLength({ min: 2 })],
+  login: [check("email").isEmail(), handleValidationErrors],
+  register: [check("email").isEmail(), checkPassword, handleValidationErrors],
   forgetPassword: [check("email").isEmail(), handleValidationErrors],
   resetPassword: [
     check("token").isJWT(),
-    check("password")
-      .isLength({ min: 2 })
-      .withMessage("Password must be at least 5 characters long"),
+    checkPassword,
     check("confirmPassword").custom((value, { req }) => {
       if (value !== req.body.password) {
-        throw new Error("Password confirmation does not match password");
+        throw new Error(translate("error.password_not_match"));
       }
       return true;
     }),
