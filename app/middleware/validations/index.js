681131cd7ff2e3c18aecd73c7b3be83710532ccb
Damirdine ALI SOILIHI <damirdine@outlook.fr>
2024-03-27 11:30:08 +0100

    fix: handleUserColocationAccess

--- app/middleware/validations/index.js
+++ app/middleware/validations/index.js
@@ -1,5 +1,6 @@
 import { validationResult } from "express-validator";
 import { error } from "../../helpers/translate.js";
+import models from "../../models/index.js";
 
 export default function handleValidationErrors(req, res, next) {
   const errors = validationResult(req);
@@ -8,14 +9,15 @@ export default function handleValidationErrors(req, res, next) {
   }
   next();
 }
-export function handleUserColocationAccess(req, res, next) {
+export async function handleUserColocationAccess(req, res, next) {
   let colocationId = req.params?.colocationID || req.params?.colocationId;
   if (!colocationId) {
     const urlPattern = /\/colocation\/(\w+)\//;
     const match = req.originalUrl.match(urlPattern);
     colocationId = match ? parseInt(match[1]) : null;
   }
-  const userColocationId = req.user.colocation_id;
+  const user = await models.user.findByPk(user.id);
+  const userColocationId = user.colocation_id;
 
   if (colocationId != userColocationId) {
     return res.status(403).json({ error: error(req).colocation_access_denied });
